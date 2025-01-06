a0bd88093a8383253f77cde93cef9cacea1257cb
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-01-06 20:09:32 +0300

    Client: Add - utils added

---
+++ src/utils/dataFormat.ts
@@ -0,0 +1,31 @@
+export function getLocalDateByISO(isoDateString: Date): string {
+  const date = new Date(isoDateString);
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const shortYear = date.getFullYear().toString().slice(-2);
+  const localTime = date.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  return `${day}.${month}.${shortYear}`;
+}
+
+export function calculateYearsAndMonths(targetDate: Date): string {
+  const today = new Date();
+  const futureDate = new Date(targetDate);
+
+  if (futureDate < today) {
+    throw new Error("Target date must be in the future.");
+  }
+
+  let years = futureDate.getFullYear() - today.getFullYear();
+  let months = futureDate.getMonth() - today.getMonth();
+
+  // Если месяцев меньше 0, уменьшить количество лет и скорректировать месяцы
+  if (months < 0) {
+    years -= 1;
+    months += 12;
+  }
+
+  return `${years}y ${months}m`;
+}
---
+++ src/utils/moexInfo.ts
@@ -0,0 +1,50 @@
+interface MoexField {
+  title: string;
+  id?: string;
+  name?: string;
+}
+
+export const MOEX_INFO_NAME: Record<string, MoexField> = {
+  total: { title: "Total" },
+  amount: { title: "Amount" },
+  createdAd: { title: "Created" },
+  updatedAd: { title: "Updated" },
+
+  shortName: { title: "Short name", id: "securities", name: "SHORTNAME" },
+  name: { title: "Name", id: "securities", name: "SECNAME" },
+  ticker: { title: "Ticker", id: "securities", name: "TICKER" },
+  currency: { title: "Currency", id: "securities", name: "FACEUNIT" },
+  price: { title: "Price", id: "marketdata", name: "LAST" },
+  nominal: { title: "Nominal", id: "securities", name: "LOTVALUE" },
+  coupon: { title: "Coupon", id: "securities", name: "ACCRUEDINT" },
+  bondYield: { title: "Yield", id: "marketdata_yields", name: "EFFECTIVEYIELD" },
+  matDate: { title: "Mat date", id: "securities", name: "MATDATE" },
+};
+
+interface MoexJson {
+  [key: string]: {
+    columns: string[];
+    data: any[];
+  };
+}
+
+export function getDataByField(moexJson: MoexJson, fieldKey: keyof typeof MOEX_INFO_NAME) {
+  const field = MOEX_INFO_NAME[fieldKey];
+  if (!field.id || !field.name) {
+    throw new Error(`Field ${fieldKey} is missing id or name`);
+  }
+  const index = moexJson[field.id].columns.indexOf(field.name);
+  if (index === -1) {
+    throw new Error(`Field ${field.name} not found in columns`);
+  }
+  return moexJson[field.id].data[0][index];
+}
+
+// const CURRENCY = {
+//     "SUR": "Рубль",
+//     "USD": "Доллар США",
+//     "EUR": "Евро",
+//     "GBP": "Фунт стерлингов Соединенного королевства",
+//     "CNY": "Китайский юань",
+//     "KZT": "Казахтанский тенге"
+// }
