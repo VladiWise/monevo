e1fe1df0162de824336d4cc1548bae754fc4a098
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-01-06 18:00:14 +0300

    Client: Style - global tailwind styles changed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,11 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      boxShadow: {
+        own: "0 5px 25px 0px rgb(0 0 0 / 0.2), 0 8px 10px -2px rgb(0 0 0 / 0.1)",
+        error:
+          "0 5px 25px 0px rgb(185 28 28 / 0.5), 0 8px 10px -2px rgb(185 28 28 / 0.1)",
+      },
     },
   },
   plugins: [],
