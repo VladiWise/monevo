c387b540a5f6d782396ab2d5ef4533bb72695abc
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-01-11 17:31:59 +0300

    Client: Update - form-elements updated

--- src/components/form-elements/index.tsx
+++ src/components/form-elements/index.tsx
@@ -17,25 +17,26 @@ export function Select({
       {...(register && { ...register(name) })}
       className="bg-white p-3 w-full rounded-xl text-gray-800 hover:border-gray-400 border-gray-100 border-2"
       {...props}
+      required
     >
-        {children}
-
+      {children}
     </select>
   );
 }
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  register?: UseFormRegister<any> ; // Updated to match react-hook-form's register return type
+  register?: UseFormRegister<any>; // Updated to match react-hook-form's register return type
   name: string; // Overriding name to make it required
 }
 
-export function Input({ register, name, value, ...props } : InputProps) {
+export function Input({ register, name, value, ...props }: InputProps) {
   return (
     <input
       className="p-3 w-full rounded-xl text-gray-800 hover:border-gray-400 border-gray-100 border-2"
       defaultValue={value}
       {...(register && { ...register(name) })}
       {...props}
+      required
     />
   );
 }
