b296a3996923119c98cb79da852f35dc88a3e0bc
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-01-24 15:29:59 +0300

    Client: Update - middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,21 +2,14 @@ import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes } from "@/paths"
 import { NextResponse } from "next/server";
-// import { getToken } from "next-auth/jwt";
 
 const { auth } = NextAuth(authConfig);
 
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 export default auth(async (req) => {
-  // const token = await getToken({ req, secret: process.env.AUTH_SECRET });
-  // console.log("TOKEN::::::", token);
 
-  const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
   const providedKey = req.headers.get("x-api-key");
-
-  console.log("API_KEY::::::::::", API_KEY);
-  console.log("providedKey::::::", providedKey);
-
 
   const { nextUrl } = req;
 
@@ -25,11 +18,6 @@ export default auth(async (req) => {
   const isApiRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-
-
-  // if (isApiRoute && providedKey !== API_KEY) {
-  //   return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  // }
 
   if (isApiRoute) {
 
@@ -51,8 +39,6 @@ export default auth(async (req) => {
   if (!isLoggedIn && !isPublicRoute) {
     return NextResponse.redirect(new URL("/auth/login", nextUrl));
   }
-
-
 
   return;
 
