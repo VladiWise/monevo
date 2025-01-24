f3d35d6707ac34703aace692793274f24f0691c1
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-01-24 22:19:42 +0300

    Client: Update - Social.tsx socials changed

--- src/components/auth/Social.tsx
+++ src/components/auth/Social.tsx
@@ -1,19 +1,42 @@
 "use client";
 
-import { FcGoogle } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
+import { signIn } from "next-auth/react";
+
+import { FaGithub, FaYandex } from "react-icons/fa";
 import { Button } from "../Button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/paths";
 
 export function Social() {
+  function onClick(provider: "github" | "yandex") {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  }
   return (
-    <section className="flex items-center w-full gap-x-2">
-      <Button variant="simple" className={"w-full"} onClick={() => {}}>
-        <FcGoogle size={24} />
-      </Button>
-
-      <Button variant="simple" className={"w-full"} onClick={() => {}}>
-        <FaGithub size={24} color="black" />
-      </Button>
-    </section>
+    <>
+      <div className="flex items-center ">
+        <div className="flex-grow border-t border-gray-300"></div>
+        <span className="mx-4 text-gray-500 text-sm">or</span>
+        <div className="flex-grow border-t border-gray-300"></div>
+      </div>
+      <section className="flex items-center w-full gap-x-2">
+        <Button
+          variant="simple"
+          className="w-full flex items-center gap-x-2"
+          onClick={() => onClick("yandex")}
+        >
+          <FaYandex size={24} color="red" /> Sign in with Yandex ID
+        </Button>
+      </section>
+      <section className="flex items-center w-full gap-x-2">
+        <Button
+          variant="simple"
+          className="w-full flex items-center gap-x-2"
+          onClick={() => onClick("github")}
+        >
+          <FaGithub size={24} color="black" /> Sign in with Github
+        </Button>
+      </section>
+    </>
   );
 }
