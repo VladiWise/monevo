ec985eb9bf77a7b02318091274d3e9423ceee271
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-01-24 22:19:09 +0300

    Client: Style - Button.tsx style changed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,14 +11,16 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
 }
 
+const fontSize = "font-medium"
+
 const variants = {
   primary:
     "bg-red-100 hover:bg-red-100/80 hover:shadow-md text-red-600 active:bg-red-100 active:shadow-none",
   secondary:
-    "bg-slate-800 hover:bg-slate-900/80 hover:shadow-md text-white active:bg-slate-900 active:shadow-none",
+    "bg-gray-800 hover:bg-gray-900/80 hover:shadow-md text-white active:bg-gray-900 active:shadow-none",
   simple:
-    "bg-gray-100 hover:bg-gray-100/80 hover:shadow-md text-slate-700 active:bg-gray-100 active:shadow-none",
-  link: "text-black hover:text-slate-700/90 hover:underline underline-offset-2",
+    "bg-gray-100 hover:bg-gray-100/80 hover:shadow-md text-gray-800 active:bg-gray-100 active:shadow-none",
+  link: "text-black hover:text-gray-700/90 hover:underline underline-offset-2",
 };
 
 export function Button({
@@ -34,7 +36,8 @@ export function Button({
       className={clsx(
         "flex justify-center p-3 min-w-fit font-bold rounded-xl",
         variants[variant],
-        className, 
+        fontSize,
+        className
       )}
       {...props}
     >
