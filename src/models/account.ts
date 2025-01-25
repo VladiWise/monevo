f21cf4701142a23d73a1ae95afb02e9ef075fb72
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-01-25 12:32:10 +0300

    Server: Update - userId from accountSchema commented out

--- src/models/account.ts
+++ src/models/account.ts
@@ -3,11 +3,11 @@ import mongoose, { Schema } from "mongoose";
 const accountSchema = new Schema({
   shortName: { type: String, required: true },
   fullName: { type: String, required: true },
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
+  // userId: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: "User",
+  //   required: true,
+  // },
 });
 
 const Account =
