8ab6a31a719ee0c61819570407ab5004c4c06964
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-01-25 15:15:38 +0300

    Client/Serever: Adjust - adjust for Vercel deploy 3

--- src/app/api/bonds/[id]/accounts/route.ts
+++ src/app/api/bonds/[id]/accounts/route.ts
@@ -8,7 +8,7 @@ export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const { id } = params; // Получаем ID фонда из URL
+  const { id } = await params; // Получаем ID фонда из URL
   const { accountId, amount, price, bondYield, matDate } = await request.json(); // Получаем accountId и amount из тела запроса
 
   await connectMongoDB();
