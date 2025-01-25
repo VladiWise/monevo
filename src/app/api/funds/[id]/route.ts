8256dc9844dac8684477029738d314fd785a1412
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-01-25 15:28:30 +0300

    Server: Update - { params }: { params: Promise<{ id: string }> } is correct (2)

--- src/app/api/funds/[id]/route.ts
+++ src/app/api/funds/[id]/route.ts
@@ -2,7 +2,7 @@ import connectMongoDB from "@/libs/mongodb";
 import Fund from "@/models/fund";
 import { NextResponse, NextRequest } from "next/server";
 
-export async function PUT(request: NextRequest, { params } : { params: {id: string}}) {
+export async function PUT(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
 
   const { accountId, name, ticker, currency, amount, price, total } = await request.json();
@@ -25,7 +25,8 @@ export async function PUT(request: NextRequest, { params } : { params: {id: stri
   );
 }
 
-export async function GET(request: NextRequest, { params } : { params : { id: string}}) {
+export async function GET({ params }: { params: Promise<{ id: string }> }) {
+
   const { id } = await params;
 
   await connectMongoDB();
