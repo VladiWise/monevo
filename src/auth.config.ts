5bf10b4ff2f44771bb8e098703cdff386a7d0bf0
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-01-27 19:42:26 +0300

    Server: Auth - Google provider added

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,32 +3,51 @@ import type { NextAuthConfig } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import { LoginSchema } from "@/schemas"
 import api from "@/libs/fetch"
-import VK from "next-auth/providers/vk"
+import Google from "next-auth/providers/google"
 import Yandex from "next-auth/providers/yandex"
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
+      // profile(profile) {
+      //   return {
+      //     id: profile.sb,
+      //     name: profile.name,
+      //     email: profile.email,
+      //     image: profile.picture,
+      //     login: profile.email ? profile.email.split("@")[0] : null,
+      //   };
+      // }
+    }),
     Yandex({
       clientId: process.env.YANDEX_CLIENT_ID,
       clientSecret: process.env.YANDEX_CLIENT_SECRET,
       authorization: {
         url: "https://oauth.yandex.ru/authorize",
         params: {
-          scope: "login:email login:info",
+          scope: "login:email login:info login:avatar",
         },
       },
+
+      // profile(profile) {
+      //   // Если аватар есть, формируем его URL
+      //   const avatarUrl = profile.default_avatar_id
+      //     ? `https://avatars.yandex.net/get-yapic/${profile.default_avatar_id}/islands-200`
+      //     : null;
+
+      //   return {
+      //     id: profile.id,
+      //     name: profile.real_name || profile.display_name,
+      //     login: profile.login,
+      //     email: profile.default_email,
+      //     image: avatarUrl, // Добавляем ссылку на аватар
+      //   };
+      // },
     }),
-    VK({
-      clientId: process.env.VK_CLIENT_ID,
-      clientSecret: process.env.VK_CLIENT_SECRET,
-      authorization: {
-        url: "https://oauth.vk.com/authorize",
-        params: {
-          scope: "vkid.personal_info",
-        },
-      },
-      checks: ["state"],
-    }),
+
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
