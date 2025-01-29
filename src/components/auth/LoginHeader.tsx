ae53b830a2265e68adc00f5ee730cc8e216c7426
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-01-29 16:27:36 +0300

    Client: Style - name changed in LoginHeader

--- src/components/auth/LoginHeader.tsx
+++ src/components/auth/LoginHeader.tsx
@@ -1,7 +1,7 @@
 export function LoginHeader({ label }: { label: string }) {
   return (
-    <section className="w-full flex flex-col gay-y-4 items-center justify-center">
-      <h1 className="text-3xl font-bold">Auth</h1>
+    <section className="w-full flex flex-col items-center justify-center gap-2">
+      <h1 className="text-2xl font-bold">FondFlow</h1>
       <p className="text-muted-foreground text-sm text-slate-700/90">{label}</p>
     </section>
   );
