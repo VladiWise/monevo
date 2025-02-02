3cee196ad73b938739d4980d4d665ce28cfc7a5a
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-02-02 12:43:07 +0300

    Client: Style - main paddings adjusted

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import AssetsTables from "./_AssetsTables";
 
 export default function App() {
   return (
-    <div className="flex flex-col items-center gap-10 p-4 max-w-6xl w-full">
+    <div className="flex flex-col items-center gap-10 sm:p-4 max-w-6xl w-full">
       <AssetsTables />
     </div>
   );
