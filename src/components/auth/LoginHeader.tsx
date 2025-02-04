e159e45d64e6ed2bad63da367d4008ec6da96ebd
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-02-04 17:49:49 +0300

    Client: Style - main name and logo on mail page changed

--- src/components/auth/LoginHeader.tsx
+++ src/components/auth/LoginHeader.tsx
@@ -4,7 +4,7 @@ export function LoginHeader({ label }: { label: string }) {
     <section className="w-full flex flex-col items-center justify-center gap-1">
       <section className="flex items-center gap">
         
-        <h1 className="text-2xl font-bold">Penny</h1>
+        <h1 className="text-2xl font-bold">Menny</h1>
         <LogoIcon size={48} />
       </section>
 
