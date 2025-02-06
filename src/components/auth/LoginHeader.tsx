7671981c14e554975433e47a7f4d7e30b62e2ca7
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-02-06 12:46:31 +0300

    Client: Style - main name (svg, png, ico) changed again :)

--- src/components/auth/LoginHeader.tsx
+++ src/components/auth/LoginHeader.tsx
@@ -2,9 +2,9 @@ import { LogoIcon } from "../SvgIcons";
 export function LoginHeader({ label }: { label: string }) {
   return (
     <section className="w-full flex flex-col items-center justify-center gap-1">
-      <section className="flex items-center gap">
+      <section className="flex items-center">
         
-        <h1 className="text-2xl font-bold">Menny</h1>
+        <h1 className="text-2xl font-bold">Monevo</h1>
         <LogoIcon size={48} />
       </section>
 
