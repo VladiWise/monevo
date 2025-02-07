1fe2a7ac414940042e2afdc01b8dedc7a81a8717
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-02-07 17:37:09 +0300

    Client: Style - manifest.ts updated, main name changed

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,14 +3,19 @@ import type { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'My Next.js PWA',
-    short_name: 'NextPWA',
-    description: 'An awesome Progressive Web App built with Next.js',
+    name: 'Monevo',
+    short_name: 'Monevo',
+    description: 'An awesome invetment tracker',
     start_url: '/',
-    display: 'standalone',
+    display: 'fullscreen',
     background_color: '#ffffff',
     theme_color: '#000000',
     icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
       {
         src: '/icon-512x512.png',
         sizes: '512x512',
--- src/components/auth/LoginHeader.tsx
+++ src/components/auth/LoginHeader.tsx
@@ -2,10 +2,12 @@ import { LogoIcon } from "../SvgIcons";
 export function LoginHeader({ label }: { label: string }) {
   return (
     <section className="w-full flex flex-col items-center justify-center gap-1">
-      <section className="flex items-center">
-        
+      <section className="flex items-center justify-center gap-1 w-full relative h-12">
         <h1 className="text-2xl font-bold">Monevo</h1>
-        <LogoIcon size={48} />
+
+        <div className="top-0 right-0 absolute">
+          <LogoIcon size={46} />
+        </div>
       </section>
 
       <p className="text-muted-foreground text-sm text-slate-700/90">{label}</p>
