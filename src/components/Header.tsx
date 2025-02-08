e24fe402f79e026956f05a1d2d869525d613ec49
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-02-08 13:42:26 +0300

    Client: Update - user image, LogoIcon, Sign out button added to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,45 @@
-"use client";
+import { signOut } from "@/auth";
+import { getCurrentUser } from "@/auth-actions/getCurrentUser";
+import Image from "next/image";
+import { LogoIcon } from "./SvgIcons";
 
-export function Header() {
+import { FaCircleUser } from "react-icons/fa6";
+import { PiSignOutDuotone } from "react-icons/pi";
+
+export async function Header() {
+  const user = await getCurrentUser();
   return (
-    <header className="sticky top-0 left-0 w-full h-14 justify-center z-50 sm:p-2 hidden md:flex bg-zinc-100 shadow-md">
-      HEADER
+    <header className="sticky top-0 left-0 w-full h-16 items-center justify-between z-50 p-2 hidden md:flex bg-zinc-100 shadow-md">
+      <section className="flex items-center gap-2">
+        <LogoIcon size={30} />
+        <p className="text-xl font-bold text-primary">MONEVO</p>
+      </section>
+
+      <section className="flex items-center gap-2">
+        <div className="flex items-center h-full">
+          {user?.image ? (
+            <Image
+              src={user?.image}
+              alt={"user"}
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+          ) : (
+            <FaCircleUser size={40} fill="#6b7280" />
+          )}
+        </div>
+
+        <form
+          action={async () => {
+            "use server";
+
+            await signOut();
+          }}
+        >
+          <button type="submit">Sing out</button>
+        </form>
+      </section>
     </header>
   );
 }
