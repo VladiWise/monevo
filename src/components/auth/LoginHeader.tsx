5104708007ffd5439fc6511701352164552e9da2
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-02-08 13:43:48 +0300

    Client: Style - styles of LogoIcon from LoginHeader updated

--- src/components/auth/LoginHeader.tsx
+++ src/components/auth/LoginHeader.tsx
@@ -3,11 +3,8 @@ export function LoginHeader({ label }: { label: string }) {
   return (
     <section className="w-full flex flex-col items-center justify-center gap-1">
       <section className="flex items-center justify-center gap-1 w-full relative h-12">
-        <h1 className="text-2xl font-bold">Monevo</h1>
-
-        <div className="top-0 right-0 absolute">
-          <LogoIcon size={46} />
-        </div>
+        <LogoIcon size={30} />
+        <p className="text-xl font-bold text-primary">MONEVO</p>
       </section>
 
       <p className="text-muted-foreground text-sm text-slate-700/90">{label}</p>
