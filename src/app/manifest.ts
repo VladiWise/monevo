6885fb5f703ef7ee8dd1fdcb780c920053e97427
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-02-22 16:58:12 +0300

    Client: Style - manifest apdated

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,8 +8,8 @@ export default function manifest(): MetadataRoute.Manifest {
     description: 'An awesome invetment tracker',
     start_url: '/',
     display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#000000',
+    background_color: 'red',
+    theme_color: 'green',
     icons: [
       {
         src: '/icon-192x192.png',
