5fb2ee7d3eea5e594389338efbe702ab74c4035d
Vladislav Kartunov <kv.vladislav04@gmail.com>
2025-04-05 20:45:53 +0300

    Client: Style - Heading style changed

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -9,7 +9,7 @@ export const Heading = ({
 }) => (
   <h1
     className={clsx(
-      "text-xl font-bold text-darkMain dark:text-white w-full",
+      "text-2xl font-bold text-darkMain dark:text-white w-full",
       className
     )}
   >
